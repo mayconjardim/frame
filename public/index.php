5db776613f691c6fffd6fd605d4fb5756284769b
Maycon Jardim <mayconsmill@gmail.com>
2024-08-10 23:41:03 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ $db = new Database;
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,6 +22,5 @@ $db = new Database;
     $rotes = new Rotes();
     include "../App/Views/layout/footer.php";
  ?>    
-
 </body>
 </html>
